2fa539751fd55d0d58fdb470b9aaeb85d954c467
오예진 Yejin Oh <yejinoh12@gmail.com>
2024-07-02 09:53:51 +0900

    [Bronze I] Title: 암호 키, Time: 1168 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1816. 암호 키/암호 키.py
@@ -0,0 +1,14 @@
+count = int(input())
+
+for _ in range(count):
+    num = int(input())
+    
+    for i in range(2,1_000_001):
+        
+        if(num%i == 0):
+            print("NO")
+            break
+        
+        if i == 1_000_000:
+            print("YES")
+            break
