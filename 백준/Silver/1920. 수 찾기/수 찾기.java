216106f761b64afd53e9f453a81a56f32bfc58f0
Yejin Oh <yejinoh12@gmail.com>
2024-07-17 01:44:08 +0900

    [Silver IV] Title: 수 찾기, Time: 1700 ms, Memory: 189664 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+
+        int m = sc.nextInt();
+        int[] findNum = new int[m];
+        for (int i = 0; i < m; i++){
+            findNum[i] = sc.nextInt();
+        }
+
+        //System.out.println("list = " + list);
+        //System.out.println("findNum = " + findNum);
+
+        for(int i=0; i< findNum.length; i++){
+            if(Arrays.binarySearch(arr, findNum[i]) >= 0){
+                System.out.println("1");
+            }else{
+                System.out.println("0");
+            }
+        }
+    }
+}
