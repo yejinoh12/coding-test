26beb8e3180685b1fe9dadc338357184eae3a295
Yejin Oh <yejinoh12@gmail.com>
2024-07-27 11:18:28 +0900

    [Silver II] Title: N과 M (9), Time: 340 ms, Memory: 29316 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,69 @@
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static int n, m;
+    public static int[] number;
+    public static boolean[] v;
+    public static List<Integer> list = new ArrayList<>();
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        n = arr[0];
+        m = arr[1];
+
+        number = new int[n];
+        v = new boolean[n];
+
+        int[] inputNum = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(inputNum);
+
+        for (int i = 0; i < n; i++) {
+            number[i] = inputNum[i];
+        }
+
+        dfs(0);
+        bw.close();
+
+    }
+
+    public static void dfs(int depth) throws IOException {
+
+        if (depth == m) {
+
+            for (int i : list) {
+                bw.write(i + " ");
+            }
+            bw.write("\n");
+
+            return;
+        }
+
+        int prev = -1;
+
+        //System.out.println(depth + " " + prev);
+
+        for (int i = 0; i < n; i++) {
+
+            if (!v[i] && prev != number[i]) {
+
+                prev = number[i];
+                v[i] = true;
+                list.add(number[i]);
+
+                dfs(depth + 1);
+
+                v[i] = false;
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+}
