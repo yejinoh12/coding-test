4f2d25c27bd561a839fdb1df7426ffd15a8b7762
Yejin Oh <yejinoh12@gmail.com>
2024-07-28 23:19:22 +0900

    [Silver II] Title: 로또, Time: 172 ms, Memory: 16060 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,36 +1,35 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 
 public class Main {
 
     static int N = 6;
     static int[] ans = new int[N];
+    static BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 
         while(true){
             int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             if(input[0] == 0) break;
             dfs(0, input[0], input,0);
-            System.out.println();
+            bw.write("\n");
         }
 
         //int[] a = new int[]{7,1,2,3,4,5,6,7};
         //dfs(0, 7, a, 0);
+        bw.close();
     }
 
-    public static void dfs(int depth, int num, int arr[], int start){
+    public static void dfs(int depth, int num, int arr[], int start) throws IOException{
 
         if(depth == N){
             for(int i : ans){
-                System.out.print(i + " ");
+                bw.write(i + " ");
             }
-            System.out.println();
+            bw.write("\n");
             return;
         }
 
