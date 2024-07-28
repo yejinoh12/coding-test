eed65deb3bbd3b6ffb9537f4931b9ca0a9038d65
Yejin Oh <yejinoh12@gmail.com>
2024-07-28 22:05:44 +0900

    [Silver II] Title: 부분수열의 합, Time: 152 ms, Memory: 14504 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -0,0 +1,63 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 부분 수열 : 연속되지 않아도 됨
+ */
+public class Main {
+
+    static int N, target;
+    static int count = 0;
+    static int sum = 0;
+    static int[] v;
+    static int num[];
+    static List<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        N = input[0]; //숫자의 범위, 동시에 깊이가 된다. 모든 조합을 봐야하기 때문에.
+        target = input[1]; //깊이가 된다.
+
+        num = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        v = new int[N];
+
+        dfs(0, 0);
+        System.out.println(count);
+
+    }
+
+    public static void dfs(int depth, int start) {
+
+        if (depth == N) {
+
+            return;
+        }
+
+
+        for (int i = start; i < N; i++) {
+
+            if (v[i] == 0) { //방문 하지 않은 경우
+
+                sum += num[i];
+                if(sum == target){
+                    //System.out.println(depth + " " + i + " " + num[i]);
+                    count++;
+                }
+
+                v[i] = 1;//방문을 했음
+                dfs(depth + 1, i + 1);
+                v[i] = 0;
+                sum -= num[i];
+            }
+
+        }
+    }
+}
