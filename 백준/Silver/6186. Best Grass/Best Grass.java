32ba01e24c5e94fa120a24adf7ac69ebbb558067
Yejin Oh <yejinoh12@gmail.com>
2024-08-01 13:40:43 +0900

    [Silver V] Title: Best Grass, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/6186. Best Grass/Best Grass.java
+++ 백준/Silver/6186. Best Grass/Best Grass.java
@@ -8,12 +8,11 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[] dx = new int[]{-1,1,0,0};
-    static int[] dy = new int[]{0,0,-1,1};
+    static int[] dx = new int[]{-1, 1, 0, 0};
+    static int[] dy = new int[]{0, 0, -1, 1};
     static int[][] v;
     static int[][] graph;
     static int row, col;
-    static int count;
 
     public static void main(String[] args) throws IOException {
 
@@ -37,10 +36,12 @@ public class Main {
             }
         }
 
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
-                if(graph[i][j] == 1 && v[i][j] == 0){
-                    bfs(i,j);
+        int count = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (graph[i][j] == 1 && v[i][j] == 0) {
+                    bfs(i, j);
+                    count++;
                 }
             }
         }
@@ -49,33 +50,20 @@ public class Main {
 
     }
 
-    public static void bfs(int x, int y){
+    public static void bfs(int x, int y) {
 
-        count ++;
-
-        Queue<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{x,y});
         v[x][y] = 1;
 
-        while(!q.isEmpty()){
+        for (int i = 0; i < 4; i++) {
 
-            int[] xy = q.poll();
-            int nowX = xy[0];
-            int nowY = xy[1];
+            int nextX = x + dx[i];
+            int nextY = y + dy[i];
 
-            for(int i = 0; i < 4; i++){
+            if (nextX < 0 || nextY < 0 || nextX >= row || nextY >= col) continue;
+            if (v[nextX][nextY] == 1 || graph[nextX][nextY] == 0) continue;
 
-                int nextX = nowX + dx[i];
-                int nextY = nowY + dy[i];
+            bfs(nextX, nextY);
 
-                if(nextX < 0 || nextY < 0 || nextX >= row || nextY >= col) continue;
-                if(v[nextX][nextY] == 1 || graph[nextX][nextY] == 0) continue;
-
-                q.offer(new int[]{nextX, nextY});
-                v[nextX][nextY] = 1;
-                //System.out.println(nextX + " " +  nextY);
-
-            }
         }
     }
 }
