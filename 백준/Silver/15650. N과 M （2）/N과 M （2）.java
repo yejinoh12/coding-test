3bc956c3f894c335b4457cdb55e459ca03a7ca6e
Yejin Oh <yejinoh12@gmail.com>
2024-10-19 17:18:23 +0900

    [Silver III] Title: N과 M (2), Time: 132 ms, Memory: 16220 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,48 +1,56 @@
+
+
+//자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+
+//1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+//고른 수열은 오름차순이어야 한다.
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Main {
-    static int n, m;
-    static List<Integer> list = new ArrayList<>();
-    static List<List<Integer>> answer = new ArrayList<>();
+
+    static int N, M;
+    static int[] arr;
+    static ArrayList<Integer> ans = new ArrayList<>();
+    static boolean[] v; //방문여부
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        N = input[0];
+        M = input[1];
 
-        n = input[0];
-        m = input[1];
+        arr = new int[N];
+        v = new boolean[N];
 
-        dfs(1, 0 );
+        backTracking(0,0);
 
-        for (List<Integer> i : answer) {
-            for (int j : i) {
-                System.out.print(j + " ");
-            }
-            System.out.println();
-        }
     }
 
-    public static void dfs(int depth, int selectedCount) {
+    public static void backTracking(int idx, int depth){
 
-        if(depth > n){
-            if (selectedCount == m) {
-                answer.add(new ArrayList<>(list));
+        //종료 조건
+        if(depth == M){
+            for(int i = 0; i < M; i++){
+                System.out.print(arr[i] + " ");
             }
+            System.out.println();
             return;
+
         }
 
-        // 현재 숫자를 선택하는 경우
-        list.add(depth);
-        dfs(depth + 1, selectedCount + 1);
-        list.remove(list.size() - 1);
-
-        // 현재 숫자를 선택하지 않는 경우
-        dfs(depth + 1, selectedCount);
+        for (int i = idx; i < N; i++) {
+            if (!v[i]) {
+                v[i] = true;
+                arr[depth] = i + 1;
+                backTracking(i + 1, depth + 1);
+                v[i] = false;
+            }
+        }
     }
 }
