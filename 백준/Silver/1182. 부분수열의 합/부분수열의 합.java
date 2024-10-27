9d69fa40c744d37c128ba85a4c78622b46bcafc4
Yejin Oh <yejinoh12@gmail.com>
2024-10-27 19:11:52 +0900

    [Silver II] Title: 부분수열의 합, Time: 124 ms, Memory: 14460 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,58 +1,44 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.io.*;
 import java.util.Arrays;
-import java.util.List;
 
-/**
- * 부분 수열 : 연속되지 않아도 됨
- */
 public class Main {
 
-    static int N, target;
-    static int count;
-    static int num[];
+    static int N, S, ans;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        N = input[0];
+        S = input[1];
 
-        N = input[0]; //숫자의 범위, 동시에 깊이가 된다. 모든 조합을 봐야하기 때문에
-        target = input[1]; //깊이가 된다.
-        num = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        dfs(0, 0);
+        backTracking(0, 0);
         
-        /**
-         * target이 0인경우, 공집합의 0도 count에 포함시키므로 
-         * 0인 경우 1을 제거한다. 
-         */
-        if(target == 0) count--;
-        System.out.println(count);
+        if (S == 0) ans--;
+
+        bw.write(ans + "");
+        bw.flush();
+        bw.close();
 
     }
 
-    public static void dfs(int depth, int sum) {
+    public static void backTracking(int depth, int sum) {
 
         if (depth == N) {
-            if (sum == target) {
-                count++;
+            if (sum == S) {
+                ans++;
             }
             return;
         }
 
-        //선택하는 경우
-        dfs(depth + 1, sum + num[depth]);
-
-        //선택하지 않는 경우
-        dfs(depth + 1, sum);
-
+        backTracking(depth + 1, sum + arr[depth]);
+        backTracking(depth + 1, sum);
 
     }
-
-
 }
-
